a0253e8fe71b102c37ffb45f2dcdf6b2063ce01e
6david9 <6david9@163.com>
2015-07-09 01:17:54 +0800

    parallel run using channel

--- parallel.go
+++ parallel.go
@@ -1,126 +1,123 @@
 package parallel
 
 import (
-	"fmt"
 	"sync"
+	"runtime"
 )
 
-type Worker func(input interface{}) (interface{}, error)
+type Worker func(input interface{}) interface{}
+type ResultHandler func(index int, result interface{})
 
-type TaskError struct {
-	Input interface{}
-	Error error
+type ParallelChannel struct {
+	taskChan      chan interface{}
+	outputChan    chan interface{}
+	quitChan      chan bool
+	parallelCount int
+	running       bool
+	worker        Worker
+	resultHandler ResultHandler
+	waitGroup     sync.WaitGroup
 }
 
-func (t *TaskError) String() string {
-	return fmt.Sprintf("%s %v", t.Input, t.Error)
-}
-
-func PrintTaskErrors(errs []TaskError) {
-	for _, err := range errs {
-		fmt.Println(err.String())
+func NewParallelChannel(count int, worker Worker, handler ResultHandler) ParallelChannel {
+	if count == 0 {
+		count = 1
+	} else if count < 0 {
+		count = runtime.NumCPU()
 	}
+	return ParallelChannel{parallelCount: count, worker: worker, resultHandler: handler, running: false}
 }
 
-type TaskChannel struct {
-	taskChan   chan interface{}
-	resultChan chan interface{}
-	errorChan  chan TaskError
+func (pc *ParallelChannel) AddTask(task interface{}) {
+	pc.taskChan <- task
 }
 
-func NewTaskChannel(tc chan interface{}, rc chan interface{}, ec chan TaskError) TaskChannel {
-	return TaskChannel{taskChan: tc, resultChan: rc, errorChan: ec}
+func (pc *ParallelChannel) TaskChan() chan interface{} {
+	return pc.taskChan
 }
 
-func (tc *TaskChannel) runInWorker(w Worker) {
-	for {
-		input, ok := <-tc.taskChan
-		if !ok {
-			break // quit when finished tasks
-		}
+func (pc *ParallelChannel) OutputChan() chan interface{} {
+	return pc.outputChan
+}
 
-		result, err := w(input)
+func (pc *ParallelChannel) Stop() {
+	close(pc.taskChan)
+	close(pc.quitChan)
+	pc.waitGroup.Wait()
+}
 
-		if err != nil {
-			if tc.errorChan != nil {
-				taskErr := TaskError{Input: input, Error: err}
-				tc.errorChan <- taskErr
-			}
-		} else {
-			tc.resultChan <- result
-		}
+func (pc *ParallelChannel) ForceStop() {
+	close(pc.taskChan)
+
+	for i := 0; i < pc.parallelCount; i++ {
+		pc.quitChan <- true
 	}
+	close(pc.quitChan)
+	pc.waitGroup.Wait()
 }
 
-func (tc *TaskChannel) asyncCollectResults(results *[]interface{}, errs *[]TaskError) {
-	// collect results
+func (pc *ParallelChannel) Run() {
+	if pc.running {
+		return
+	}
+	pc.running = true
+
+	pc.taskChan = make(chan interface{}, pc.parallelCount)
+	pc.outputChan = make(chan interface{}, pc.parallelCount)
+	pc.quitChan = make(chan bool, pc.parallelCount)
+
+	pc.waitGroup.Add(1)
+	pc.startHandleResults()
+
 	go func() {
+		var wg sync.WaitGroup
+
+		for i := 0; i < pc.parallelCount; i++ {
+			wg.Add(1)
+
+			go func() {
+			LOOP:
+				for {
+					select {
+					case <-pc.quitChan: // 用户显式的退出
+						wg.Done()
+						break LOOP
+
+					case task, ok := <-pc.taskChan:
+						if !ok { // 没有任务后退出
+							wg.Done()
+							break LOOP
+						}
+
+						result := pc.worker(task)
+						pc.outputChan <- result
+
+					default:
+						wg.Done()
+						break LOOP
+					}
+				}
+			}()
+		}
+		wg.Wait()
+
+		close(pc.outputChan)
+
+		pc.running = false
+		pc.waitGroup.Done()
+	}()
+}
+
+func (pc *ParallelChannel) startHandleResults() {
+	go func() {
+		index := 0
 		for {
-			result, ok := <-tc.resultChan
+			result, ok := <-pc.outputChan
 			if !ok {
 				break
 			}
-			*results = append(*results, result)
+			pc.resultHandler(index, result)
+			index += 1
 		}
 	}()
-
-	// collect errors
-	go func() {
-		for {
-			retErr, ok := <-tc.errorChan
-			if !ok {
-				break
-			}
-			*errs = append(*errs, retErr)
-		}
-	}()
-}
-
-func ParallelRun(limit int, inputs []interface{}, w Worker) ([]interface{}, []TaskError) {
-	count := len(inputs)
-
-	if limit < 1 || limit > count {
-		limit = count
-	}
-
-	taskChan := make(chan interface{}, count)
-	resultChan := make(chan interface{}, count)
-	errorChan := make(chan TaskError, count)
-
-	// dispatch tasks
-	go func() {
-		for _, input := range inputs {
-			taskChan <- input
-		}
-		close(taskChan)
-	}()
-
-	tc := NewTaskChannel(taskChan, resultChan, errorChan)
-
-	// schedule goroutines to collect results and errors
-	results := make([]interface{}, 0, count)
-	errs := make([]TaskError, 0)
-	tc.asyncCollectResults(&results, &errs)
-
-	// start #limit workers
-	var taskWG sync.WaitGroup
-	for i := 0; i < limit; i++ {
-		// id := i
-		taskWG.Add(1)
-		go func() {
-			tc.runInWorker(w)
-			// fmt.Printf("(%d) Done!\n", id)
-			taskWG.Done()
-		}()
-	}
-	taskWG.Wait()
-
-	// after below lines resultChan and errorChan will be closed.
-	// when other goroutines write to these channel it would be crashed
-	// and it should be crashed. because till to this line, sub-goroutines should
-	// have finished running tasks.
-	close(resultChan)
-	close(errorChan)
-
-	return results, errs
 }
